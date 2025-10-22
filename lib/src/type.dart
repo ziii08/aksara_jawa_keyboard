9f8edd0a52df6cb9c3b70714a266567370ab95e8
ziiii08 <fauzi08.dev@gmail.com>
2025-10-22 13:35:20 +0700

    fix: remove type unused

--- lib/src/type.dart
+++
@@ -1,6 +0,0 @@
-part of in_app_keyboard;
-
-/// Available Virtual Keyboard Types:
-/// `Numeric` - Numeric only.
-/// `Alphanumeric` - Alphanumeric: letters`[A-Z]` + numbers`[0-9]` + `@` + `.`.
-enum KeyboardType { Numeric, Alphanumeric }
