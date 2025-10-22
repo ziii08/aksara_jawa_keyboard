d4c98a69057fb006df91875da4953b2c3fc25c00
ziiii08 <fauzi08.dev@gmail.com>
2025-10-22 09:56:29 +0700

    feat: upgrade version of flutter example project to 3.35.4

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\src\flutter\fvm\versions\3.35.4"
+export "FLUTTER_APPLICATION_PATH=D:\work\project\in_app_keyboard\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
